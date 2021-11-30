86253f808dd116b0f3d7efe0eb0956a877bdb5da
GiantBooley <95261674+GiantBooley@users.noreply.github.com>
2021-11-30 08:30:51 -0500

    Update unblok website.js

--- unblok website.js
+++ unblok website.js
@@ -1,1 +1,1 @@
-javascript:var val = prompt("enter the url to unblock.", "");location = "http://webcache.googleusercontent.com/search?q=cache:" + escape(val);
+javascript:var unblockedUrl = prompt("enter the url to unblock.");location = "http://webcache.googleusercontent.com/search?q=cache:" + escape(unblockedUrl);
