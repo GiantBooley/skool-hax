d951055012b8c2a5a9f9d1b08f95435a990ade53
GiantBooley <95261674+GiantBooley@users.noreply.github.com>
2021-11-30 21:37:59 -0500

    updated syntax
    bif

--- developer console.js
+++ developer console.js
@@ -1,1 +1,1 @@
-javascript:var x = document.createElement("script"); x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js"; x.onload = alert("Loaded Developer Console!"); document.head.appendChild(x);
+javascript:(()=>{var x = document.createElement("script"); x.src = "https://cdn.jsdelivr.net/gh/SnowLord7/devconsole@master/main.js"; x.onload = alert("Loaded Developer Console!"); document.head.appendChild(x);})();
