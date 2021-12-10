91a93e301eb1fd1499f91dba341a37aa941ae310
GiantBooley <95261674+GiantBooley@users.noreply.github.com>
2021-12-10 00:36:55 -0500

    Update fake terminal.js

--- fake terminal.js
+++ fake terminal.js
@@ -1,1 +1,1 @@
-javascript:(()=>{document.body.style.backgroundColor = "black";document.body.contentEditable = 'true'; document.designMode='on'; void 0;})();
+javascript:(()=>{document.body.style.backgroundColor = "black";document.body.contentEditable = 'true'; document.designMode='on'; void 0; document.body.style.color="green"})();
